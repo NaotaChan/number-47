a879e3256df7cfcf9f759f9eaeaf6af479443443
Roberto Golia <robertogolia17@gmail.com>
2026-07-15 16:26:45 +0200

    feat: base DialogueManager loop with keyboard input 1/2/3, stress update, next customer

--- Assets/_Project/Scripts/Managers/DialogueManager.cs
+++ Assets/_Project/Scripts/Managers/DialogueManager.cs
@@ -4,6 +4,9 @@ public class DialogueManager : MonoBehaviour
 {
 
     [SerializeField]
+    GameManager gameManager;
+
+    [SerializeField]
     CustomerManager customerManager;
 
 
@@ -20,5 +23,27 @@ public class DialogueManager : MonoBehaviour
     void Update()
     {
         
+
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Debug.Log(customerManager.CurrentCustomer.CustomerResponses[0].text);
+
+            gameManager.MentalStateSystem.ModifyStress(customerManager.CurrentCustomer.CustomerResponses[0].stressEffect);
+            customerManager.NextCustomer();
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Debug.Log(customerManager.CurrentCustomer.CustomerResponses[1].text);
+
+            gameManager.MentalStateSystem.ModifyStress(customerManager.CurrentCustomer.CustomerResponses[1].stressEffect);
+            customerManager.NextCustomer();
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Debug.Log(customerManager.CurrentCustomer.CustomerResponses[2].text);
+
+            gameManager.MentalStateSystem.ModifyStress(customerManager.CurrentCustomer.CustomerResponses[2].stressEffect);
+            customerManager.NextCustomer();
+        }
     }
 }
