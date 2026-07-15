2a16674a2178dc4f9e6473199067eb5794a4b835
Roberto Golia <robertogolia17@gmail.com>
2026-07-15 16:47:25 +0200

    feat: menu navigation and pause menu

---
+++ Assets/_Project/Scripts/Managers/MainMenuManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+
+    public void OpenCredits()
+    {
+        SceneManager.LoadScene("CreditsScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("QuitGame Called");
+    }
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
